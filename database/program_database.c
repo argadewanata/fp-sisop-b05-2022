48e07b548254c461af2c03351210318da804ef1a
Rere Arga Dewanata <rerearga@gmail.com>
2022-05-31 11:34:07 +0000

    Update client/program_client.c, database/program_database.c, dump/program_dump_client.c

--- database/program_database.c
+++ database/program_database.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <sys/stat.h> 
+#include <dirent.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main ()
+{
+
+    return 0;
+}
+
+
 
